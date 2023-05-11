2c341fd69d4adb369a53783fe28fb1a371853313
bel4enok03 <belskaya-tanyush@mail.ru>
2023-05-11 15:59:42 +0300

    *

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
 import Header from './components/Header/Header';
+import Form from './components/Form/Form';
 
 function App() {
 	const { tg } = useTelegram();
@@ -18,7 +18,7 @@ function App() {
 			<Header/>
 			<Routes>
 				<Route index element={<ProductList/>} />
-				<Route path={'/form'} element={<Form />} />
+				<Route path={'form'} element={<Form/>} />
 			</Routes>
 		</div>
 	);
