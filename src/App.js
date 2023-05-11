182d8ebad63417758424a20ff67df2aa0afc8e1a
bel4enok03 <belskaya-tanyush@mail.ru>
2023-05-11 15:41:08 +0300

    *

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 
 function App() {
-	const { onToggleButton, tg } = useTelegram();
+	const { tg } = useTelegram();
 
 	useEffect(() => {
 		tg.ready();
@@ -16,7 +16,6 @@ function App() {
 	return (
 		<div className="App">
 			<Header/>
-      <button onClick={onToggleButton}>toggle</button>
 			<Routes>
 				<Route index element={<ProductList/>} />
 				<Route path={'form'} element={<Form />} />
